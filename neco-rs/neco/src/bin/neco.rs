62ab5ba75fbe867e0206922b2a79f7c575fa9e57
Masato Mori <13274198+spica314@users.noreply.github.com>
2023-02-12 01:59:00 +0900

    Lisp-likeにしていた時代のコードを削除

--- neco-rs/neco/src/bin/neco.rs
+++ neco-rs/neco/src/bin/neco.rs
@@ -1,8 +1,1 @@
-use neco::{run_cli, CliContext};
-
-fn main() {
-    let args: Vec<_> = std::env::args().collect();
-    let filename = &args[1];
-    let context = CliContext::Compile(filename.to_string());
-    run_cli(context);
-}
+fn main() {}
--- neco-rs/neco/tests/test_001.rs
+++ neco-rs/neco/tests/test_001.rs
@@ -1,7 +1,1 @@
-use neco::{run_cli, CliContext};
 
-#[test]
-fn test_001() {
-    let context = CliContext::Compile("../../library/wip/prop.fe".into());
-    run_cli(context);
-}

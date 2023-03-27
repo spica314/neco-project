8c734dbc89496af389477eeae56cf89351b1847b
Masato Mori <13274198+spica314@users.noreply.github.com>
2023-03-28 02:46:52 +0900

    PathResolverを追加

--- neco-rs/felis-rename/src/lib.rs
+++ neco-rs/felis-rename/src/lib.rs
@@ -1,6 +1,7 @@
 use felis_syn::SynTreeId;
 use neco_table::{define_wrapper_of_table, define_wrapper_of_table_id};
 
+pub mod path_resolver;
 pub mod rename_defs;
 pub mod rename_uses;
 
---
+++ neco-rs/felis-rename/src/path_resolver.rs
@@ -0,0 +1,65 @@
+use std::collections::HashMap;
+
+use crate::SerialId;
+
+pub struct PathResolver {
+    children: HashMap<String, PathResolver>,
+    items: HashMap<String, SerialId>,
+}
+
+impl PathResolver {
+    pub fn new() -> PathResolver {
+        PathResolver {
+            children: HashMap::new(),
+            items: HashMap::new(),
+        }
+    }
+    pub fn get(&self, path: &[String]) -> Option<SerialId> {
+        assert!(!path.is_empty());
+        if path.len() == 1 {
+            self.items.get(&path[0]).cloned()
+        } else {
+            self.children
+                .get(&path[0])
+                .map(|child| child.get(&path[1..]))
+                .flatten()
+        }
+    }
+    pub fn set(&mut self, path: &[String], id: SerialId) {
+        assert!(!path.is_empty());
+        if path.len() == 1 {
+            self.items.insert(path[0].clone(), id);
+        } else {
+            self.children
+                .entry(path[0].clone())
+                .or_insert_with(|| PathResolver::new())
+                .set(&path[1..], id);
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::SerialId;
+
+    use super::PathResolver;
+
+    #[test]
+    fn test_path_resolver_1() {
+        let mut resolver = PathResolver::new();
+        let id1 = SerialId::new();
+        resolver.set(&["foo".to_string()], id1);
+        assert_eq!(resolver.get(&["foo".to_string()]), Some(id1));
+    }
+
+    #[test]
+    fn test_path_resolver_2() {
+        let mut resolver = PathResolver::new();
+        let id1 = SerialId::new();
+        resolver.set(&["foo".to_string(), "bar".to_string()], id1);
+        assert_eq!(
+            resolver.get(&["foo".to_string(), "bar".to_string()]),
+            Some(id1)
+        );
+    }
+}

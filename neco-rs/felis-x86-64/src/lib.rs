28b16f2add3a77e317af071996e495994d9c0164
Masato Mori <13274198+spica314@users.noreply.github.com>
2023-12-18 00:07:10 +0900

    Support examples/let-string

--- neco-rs/felis-x86-64/src/lib.rs
+++ neco-rs/felis-x86-64/src/lib.rs
@@ -1,9 +1,10 @@
+use felis_code_gen_prepared::CodeGenPreparedDecoration;
+use felis_rename::rename_defs::DefId;
 use felis_syn::{
-    syn_expr::SynExpr,
+    syn_expr::{SynExpr, SynExprApp, SynExprIdentWithPath, SynExprString},
     syn_file::{SynFile, SynFileItem},
     syn_statement::SynStatement,
 };
-use felis_type_checker::typing::TypedDecoration;
 
 fn string_length(s: &str) -> usize {
     let cs: Vec<_> = s.chars().collect();
@@ -19,8 +20,93 @@ fn string_length(s: &str) -> usize {
     res
 }
 
-pub fn compile(file: SynFile<TypedDecoration>) -> String {
-    let mut res = String::new();
+pub struct CompileContext {
+    pub res: String,
+    pub strings: Vec<(String, String)>,
+    pub def_id_to_offset: std::collections::HashMap<DefId, usize>,
+}
+
+pub fn compile_expr_app(
+    context: &mut CompileContext,
+    expr_app: &SynExprApp<CodeGenPreparedDecoration>,
+) {
+    let app = expr_app;
+    let fun = app.exprs[0].clone();
+    let SynExpr::IdentWithPath(fun) = fun else {
+        panic!()
+    };
+    let fun = fun.ident.as_str();
+    if fun == "__write_to_stdout" {
+        let arg = app.exprs[1].clone();
+        compile_expr(context, &arg);
+        context.res.push_str("    mov rax, 1\n");
+        context.res.push_str("    mov rdi, 1\n");
+        context.res.push_str("    pop rsi\n");
+        context.res.push_str("    pop rdx\n");
+        context.res.push_str("    syscall\n");
+    } else {
+        panic!();
+    }
+}
+
+pub fn compile_expr_string(
+    context: &mut CompileContext,
+    expr_string: &SynExprString<CodeGenPreparedDecoration>,
+) {
+    let s = expr_string;
+    let s = s.token_string.string.clone();
+    let label = format!("__string_{}", context.strings.len());
+    context.strings.push((label.clone(), s.clone()));
+    context
+        .res
+        .push_str(&format!("    mov rax, {}\n", string_length(&s)));
+    context.res.push_str("    push rax\n");
+    context
+        .res
+        .push_str(format!("    lea rax, {}\n", label).as_str());
+    context.res.push_str("    push rax\n");
+    context.res.push_str("    syscall\n");
+}
+
+pub fn compile_expr_ident_with_path(
+    context: &mut CompileContext,
+    expr_ident_with_path: &SynExprIdentWithPath<CodeGenPreparedDecoration>,
+) {
+    let def_id = expr_ident_with_path.ext.use_id;
+    let offset = context.def_id_to_offset.get(&def_id).unwrap();
+    context
+        .res
+        .push_str(format!("    mov rax, [rbp-{}-8]\n", offset).as_str());
+    context.res.push_str("    push rax\n");
+    context
+        .res
+        .push_str(format!("    mov rax, [rbp-{}-16]\n", offset).as_str());
+    context.res.push_str("    push rax\n");
+}
+
+pub fn compile_expr(context: &mut CompileContext, expr: &SynExpr<CodeGenPreparedDecoration>) {
+    match expr {
+        SynExpr::IdentWithPath(expr_ident_with_path) => {
+            compile_expr_ident_with_path(context, expr_ident_with_path);
+        }
+        SynExpr::App(expr_app) => {
+            compile_expr_app(context, expr_app);
+        }
+        SynExpr::Match(_) => todo!(),
+        SynExpr::Paren(_) => todo!(),
+        SynExpr::String(expr_string) => {
+            compile_expr_string(context, expr_string);
+        }
+        SynExpr::Block(_) => todo!(),
+    }
+}
+
+pub fn compile(file: SynFile<CodeGenPreparedDecoration>) -> String {
+    let mut context = CompileContext {
+        res: String::new(),
+        strings: vec![],
+        def_id_to_offset: std::collections::HashMap::new(),
+    };
 
     // get entrypoint
     let mut entrypoint_name = None;
@@ -37,7 +123,7 @@ pub fn compile(file: SynFile<TypedDecoration>) -> String {
     }
 
     // asm template
-    res.push_str(
+    context.res.push_str(
         format!(
             r#"
 .intel_syntax noprefix
@@ -59,8 +145,6 @@ _start:
         .as_str(),
     );
 
-    let mut strings = vec![];
-
     // Proc
     for item in &file.items {
         let proc = match item {
@@ -68,78 +152,62 @@ _start:
             _ => continue,
         };
 
-        res.push_str(format!("{}_{}:\n", proc.name.as_str(), proc.ext.id.as_usize()).as_str());
+        context
+            .res
+            .push_str(format!("{}_{}:\n", proc.name.as_str(), proc.ext.id.as_usize()).as_str());
 
-        res.push_str("    push rbp\n");
-        res.push_str("    mov rbp, rsp\n");
+        context.res.push_str("    push rbp\n");
+        context.res.push_str("    mov rbp, rsp\n");
+
+        context.def_id_to_offset.clear();
+        let mut offset = 0;
+        for a in &proc.ext.lets {
+            context.def_id_to_offset.insert(a.0, offset);
+            offset += 16;
+        }
+
+        context.res.push_str(&format!("    sub rsp, {}\n", offset));
 
         for statement in &proc.proc_block.statements {
             match statement {
-                SynStatement::Expr(SynExpr::App(app)) => {
-                    let fun = app.exprs[0].clone();
-                    let SynExpr::IdentWithPath(fun) = fun else {
-                        panic!("fun = {:?}", fun)
-                    };
-                    let fun = fun.ident.as_str();
-                    if fun == "__write_to_stdout" {
-                        let arg = app.exprs[1].clone();
-                        let SynExpr::String(s) = arg else { panic!() };
-                        let s = s.token_string.string.clone();
-                        let label = format!("__string_{}", strings.len());
-                        strings.push((label.clone(), s.clone()));
-                        res.push_str("    mov rax, 1\n");
-                        res.push_str("    mov rdi, 1\n");
-                        res.push_str(format!("    lea rsi, {}\n", label).as_str());
-                        res.push_str(format!("    mov rdx, {}\n", string_length(&s)).as_str());
-                        res.push_str("    syscall\n");
-                    } else {
-                        panic!();
-                    }
+                SynStatement::Expr(expr) => {
+                    compile_expr(&mut context, expr);
                 }
                 SynStatement::ExprSemi(expr_semi) => {
                     let expr = expr_semi.expr.clone();
-                    let SynExpr::App(app) = expr else { panic!() };
-                    let fun = app.exprs[0].clone();
-                    let SynExpr::IdentWithPath(fun) = fun else {
-                        panic!()
-                    };
-                    let fun = fun.ident.as_str();
-                    if fun == "__write_to_stdout" {
-                        let arg = app.exprs[1].clone();
-                        let SynExpr::String(s) = arg else { panic!() };
-                        let s = s.token_string.string.clone();
-                        let label = format!("__string_{}", strings.len());
-                        strings.push((label.clone(), s.clone()));
-                        res.push_str("    mov rax, 1\n");
-                        res.push_str("    mov rdi, 1\n");
-                        res.push_str(format!("    lea rsi, {}\n", label).as_str());
-                        res.push_str(format!("    mov rdx, {}\n", string_length(&s)).as_str());
-                        res.push_str("    syscall\n");
-                    } else {
-                        panic!();
-                    }
+                    compile_expr(&mut context, &expr);
                 }
-                SynStatement::Let(_statement_let) => {
-                    panic!()
+                SynStatement::Let(statement_let) => {
+                    compile_expr(&mut context, &statement_let.expr);
+                    let offset = context.def_id_to_offset.get(&statement_let.ext.id).unwrap();
+                    context.res.push_str("    pop rax\n");
+                    context
+                        .res
+                        .push_str(format!("    mov [rbp-{}-16], rax\n", offset).as_str());
+                    context.res.push_str("    pop rax\n");
+                    context
+                        .res
+                        .push_str(format!("    mov [rbp-{}-8], rax\n", offset).as_str());
                 }
-                _ => panic!(),
             }
         }
 
-        res.push_str("    mov rsp, rbp\n");
-        res.push_str("    pop rbp\n");
-        res.push_str("    mov rax, 0\n");
-        res.push_str("    ret\n");
+        context.res.push_str("    mov rsp, rbp\n");
+        context.res.push_str("    pop rbp\n");
+        context.res.push_str("    mov rax, 0\n");
+        context.res.push_str("    ret\n");
     }
 
     // string literals
-    res.push_str("    .section .data\n");
-    for (label, s) in strings {
-        res.push_str(format!("{}:\n", label).as_str());
-        res.push_str(format!("    .asciz \"{}\"\n", s).as_str());
+    context.res.push_str("    .section .data\n");
+    for (label, s) in &context.strings {
+        context.res.push_str(format!("{}:\n", label).as_str());
+        context
+            .res
+            .push_str(format!("    .asciz \"{}\"\n", s).as_str());
     }
 
-    res
+    context.res
 }
 
 #[cfg(test)]
@@ -187,7 +255,9 @@ mod test {
         assert_eq!(var_types.len(), 2);
         let file_4 = typing_file(&mut context, &file_3);
 
-        let res = compile(file_4);
+        let file_5 = felis_code_gen_prepared::prepare_code_gen_file(&file_4);
+
+        let res = compile(file_5);
         let base_path = format!("/tmp/{}", test_name_to_file_name("felis_compile_test_1"));
         {
             let base_path = Path::new(&base_path);
@@ -223,62 +293,64 @@ mod test {
         assert_eq!(output.status.code(), Some(0));
     }
 
-    // #[test]
-    // fn felis_compile_test_2() {
-    //     let s = std::fs::read_to_string("../../examples/let-string/main.fe").unwrap();
-    //     let file = parse_from_str::<SynFile<UD>>(&s).unwrap().unwrap();
-    //     let mut context = RenameDefContext::new();
-    //     let file_2 = rename_defs_file(&mut context, &file).unwrap();
-    //     let mut resolver = Resolver::new();
-    //     let path_table = construct_path_table_syn_file(&file_2).unwrap();
-    //     path_table.setup_resolver(file_2.ext.id, &mut resolver);
-    //     let prelude_map = setup_resolver_for_prelude(&mut context, &mut resolver);
-    //     let mut context2 = RenameUseContext::new(resolver, path_table);
-    //     let file_3 = rename_uses_file(&mut context2, &file_2).unwrap();
+    #[test]
+    fn felis_compile_test_2() {
+        let s = std::fs::read_to_string("../../examples/let-string/main.fe").unwrap();
+        let file = parse_from_str::<SynFile<UD>>(&s).unwrap().unwrap();
+        let mut context = RenameDefContext::new();
+        let file_2 = rename_defs_file(&mut context, &file).unwrap();
+        let mut resolver = Resolver::new();
+        let path_table = construct_path_table_syn_file(&file_2).unwrap();
+        path_table.setup_resolver(file_2.ext.id, &mut resolver);
+        let prelude_map = setup_resolver_for_prelude(&mut context, &mut resolver);
+        let mut context2 = RenameUseContext::new(resolver, path_table);
+        let file_3 = rename_uses_file(&mut context2, &file_2).unwrap();
 
-    //     let mut context = setup_retrieve_context(&prelude_map);
-    //     retrieve_file(&mut context, &file_3);
-    //     context.type_checker.resolve();
-    //     let var_types = context.type_checker.get_all();
-    //     for (var_id, ty) in &var_types {
-    //         eprintln!("{:?}: {:?}", var_id, ty);
-    //     }
-    //     assert_eq!(var_types.len(), 4);
-    //     let file_4 = typing_file(&mut context, &file_3);
+        let mut context = setup_retrieve_context(&prelude_map);
+        retrieve_file(&mut context, &file_3);
+        context.type_checker.resolve();
+        let var_types = context.type_checker.get_all();
+        for (var_id, ty) in &var_types {
+            eprintln!("{:?}: {:?}", var_id, ty);
+        }
+        assert_eq!(var_types.len(), 4);
+        let file_4 = typing_file(&mut context, &file_3);
 
-    //     let res = compile(file_4);
-    //     let base_path = format!("/tmp/{}", test_name_to_file_name("felis_compile_test_2"));
-    //     {
-    //         let base_path = Path::new(&base_path);
-    //         if base_path.exists() {
-    //             std::fs::remove_dir_all(&base_path).unwrap();
-    //         }
-    //     }
-    //     std::fs::create_dir(&base_path).unwrap();
-    //     std::fs::write(format!("{}/main.s", base_path), &res).unwrap();
+        let file_5 = felis_code_gen_prepared::prepare_code_gen_file(&file_4);
 
-    //     let status = Command::new("as")
-    //         .args(&[
-    //             "-o",
-    //             &format!("{}/main.o", base_path),
-    //             &format!("{}/main.s", base_path),
-    //         ])
-    //         .status()
-    //         .expect("");
-    //     assert_eq!(status.code(), Some(0));
-    //     let status = Command::new("ld")
-    //         .args(&[
-    //             "-o",
-    //             &format!("{}/a.out", base_path),
-    //             &format!("{}/main.o", base_path),
-    //         ])
-    //         .status()
-    //         .expect("");
-    //     assert_eq!(status.code(), Some(0));
-    //     let output = Command::new(&format!("{}/a.out", base_path))
-    //         .output()
-    //         .expect("");
-    //     assert_eq!(output.stdout, b"Hello, world!\n");
-    //     assert_eq!(output.status.code(), Some(0));
-    // }
+        let res = compile(file_5);
+        let base_path = format!("/tmp/{}", test_name_to_file_name("felis_compile_test_2"));
+        {
+            let base_path = Path::new(&base_path);
+            if base_path.exists() {
+                std::fs::remove_dir_all(&base_path).unwrap();
+            }
+        }
+        std::fs::create_dir(&base_path).unwrap();
+        std::fs::write(format!("{}/main.s", base_path), &res).unwrap();
+
+        let status = Command::new("as")
+            .args(&[
+                "-o",
+                &format!("{}/main.o", base_path),
+                &format!("{}/main.s", base_path),
+            ])
+            .status()
+            .expect("");
+        assert_eq!(status.code(), Some(0));
+        let status = Command::new("ld")
+            .args(&[
+                "-o",
+                &format!("{}/a.out", base_path),
+                &format!("{}/main.o", base_path),
+            ])
+            .status()
+            .expect("");
+        assert_eq!(status.code(), Some(0));
+        let output = Command::new(&format!("{}/a.out", base_path))
+            .output()
+            .expect("");
+        assert_eq!(output.stdout, b"Hello, world!\n");
+        assert_eq!(output.status.code(), Some(0));
+    }
 }
